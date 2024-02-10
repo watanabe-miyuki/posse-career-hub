e91711632447b193a20673be4a40b03c358c6142
watanabe-miyuki <kirin.myk2018@gmail.com>
2024-02-10 20:55:51 +0900

    feat: :art: discord認証

--- app/lib/next-auth/options.ts
+++ app/lib/next-auth/options.ts
@@ -1,21 +1,28 @@
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+// import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./prisma";
+import DiscordProvider from "next-auth/providers/discord";
 
 export const nextAuthOptions: NextAuthOptions = {
-  debug: false,
+  debug: true,
   session: { strategy: "jwt" },
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    // 同じめ
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID!,
+    //   clientSecret: process.env.GITHUB_SECRET!,
+    // }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_ID!,
+      clientSecret: process.env.DISCORD_SECRET!,
     }),
   ],
   // githubで認証したあとprismaのユーザーに保存される
   adapter: PrismaAdapter(prisma),
   callbacks: {
     session: ({ session }) => {
+        console.log('is session', session); 
       return {
         ...session,
       };
