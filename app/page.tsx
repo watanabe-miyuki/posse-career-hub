2b0f31cf9ed2e7308aa59c80e4b59904050fd0ca
watanabe-miyuki <kirin.myk2018@gmail.com>
2024-02-17 09:02:03 +0900

    feat: :art: hero 作成

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Card from "./components/Card";
 import { nextAuthOptions } from "./lib/next-auth/options";
 import { CardType, User } from "./types/types";
 import { headers } from "next/headers";
+import Image from "next/image";
 
 export default async function Home() {
   // use session はサーバーサイドでしか使えないので、ここで使うことはできない
@@ -25,10 +26,27 @@ export default async function Home() {
 
   const contents = await response.json();
 
+  const heroImage = [
+    "/thumbnails/friends.jpg",
+    "/thumbnails/group.jpg",
+    "/thumbnails/study.jpg",
+    "/thumbnails/friends.jpg",
+    "/thumbnails/group.jpg",
+  ];
+
   return (
     <>
-      <main className="md:mt-32 mt-20 lg:mx-80">
-        <h2 className="text-center w-full font-bold text-3xl mb-2">OB訪問</h2>
+      <div className="relative flex w-full overflow-hidden items-center justify-center">
+        {heroImage.map((image, index) => (
+          <Image key={index} src={image} alt="hero" width={400} height={100} />
+        ))}
+        <div className="absolute top-0 left-0 w-full h-full bg-[#13100d99]"></div>
+        <div className="absolute text-white text-3xl font-bold flex flex-col items-center">
+          OBOGと繋がる in POSSE
+          <p className="text-lg font-normal">POSSE よろず相談所</p>
+        </div>
+      </div>
+      <main className="md:mt-20 mt-10 lg:mx-80">
         <div className="columns-2 md:columns-3">
           {contents.map((card: CardType) => (
             <div key={card.id} className="break-inside-avoid mx-3 mb-6">
